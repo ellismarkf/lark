e1fa96ee1076a41aa11df936b00f5aa66d003299
Mark Ellis <ellismarkf@gmail.com>
2014-07-29 18:08:24 -0500

    add beta JSON data fetching from searchCtlr

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,23 @@
 
 var vultureControllers = angular.module('vultureControllers', []);
 
-vultureControllers.controller('searchCtlr', ['$scope', 'Destination'
+vultureControllers.controller('searchCtlr', ['$scope', 'Destination',
   function($scope, Destination) {
-    $scope.cities = Destination.query();
+
+    Destination.query().$promise.then(function (tripData) {
+      console.log('Trip data:', tripData);
+
+      var result = [];
+      // Massage tripData and put into result
+
+      for(var i = 0; i < tripData.length; i += 1) {
+        var option = {};
+        option.destination = tripData[i].trips.tripOption[0].pricing[0].fare[0].destination;
+        option.price = tripData[i].trips.tripOption[0].saleTotal;
+        console.log('Option:', option);
+        result.push(option);
+      };
+
+      $scope.trips = result;
+    });
   }]);
