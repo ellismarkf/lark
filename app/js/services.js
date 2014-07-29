f9c878e380bc2c73e1c92034449b326f30220c43
Mark Ellis <ellismarkf@gmail.com>
2014-07-29 16:33:07 -0500

    add service to connect to city JSON files

--- app/js/services.js
+++ app/js/services.js
@@ -1,1 +1,10 @@
 'use strict';
+
+var vultureServices = angular.module('vultureServices', ['ngResource']);
+
+vultureServices.factory('Destination', ['$resource',
+  function($resource) {
+    return $resource('cities/:city.json', {}, {
+      query: {method: 'GET', params:{city: 'all'}, isArray:true}
+    });
+  }]);
