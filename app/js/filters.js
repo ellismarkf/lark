3d63c210fe87d77cea94d28796b6c9adaabc6fcc
Mark Ellis <ellismarkf@gmail.com>
2014-07-30 19:23:11 -0500

    add priceMax filter

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,18 @@
 'use strict';
+
+var vultureFilters = angular.module('vultureFilters', []);
+
+vultureFilters.filter('priceMax', function() {
+  return function(trips, number) {
+    var cheaper = [];
+    for(var i = 0; i < trips.length; i++){
+      var trip = trips[i];
+      if(number == null){
+        return trips;
+      } else if(trip.price < number){
+        cheaper.push(trip);
+      }
+    }
+    return cheaper;
+  };
+});
