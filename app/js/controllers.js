e13ea8c5b01909ebbb0b7f4ecfcb67a6e7b61328
Mark Ellis <ellismarkf@gmail.com>
2014-07-30 19:26:55 -0500

    add city code service, convert trip prices to integers from numbers, DRY up trip object property definitions

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,18 @@
 
 var vultureControllers = angular.module('vultureControllers', []);
 
-vultureControllers.controller('searchCtlr', ['$scope', 'Destination',
-  function($scope, Destination) {
+vultureControllers.controller('searchCtlr', ['$scope', 'Destination', 'CityCode',
+  function($scope, Destination, CityCode) {
+
+    var c = {};
+    CityCode.get().$promise.then(function (cities){
+      for(var city in cities){
+        c[city] = cities[city];
+      }
+      console.log("cities: ", c);
+      return c;
+    });
+
 
     Destination.query().$promise.then(function (tripData) {
       console.log('Trip data:', tripData);
@@ -12,13 +22,20 @@ vultureControllers.controller('searchCtlr', ['$scope', 'Destination',
       // Massage tripData and put into result
 
       for(var i = 0; i < tripData.length; i += 1) {
+        var destination = tripData[i].trips.tripOption[0].pricing[0].fare;
         var option = {};
-        option.destination = tripData[i].trips.tripOption[0].pricing[0].fare[0].destination;
-        option.price = tripData[i].trips.tripOption[0].saleTotal;
+        option.destination = tripData[i].trips.tripOption[0].pricing[0].fare[tripData[i].trips.tripOption[0].pricing[0].fare.length - 1].destination;
+        var price = tripData[i].trips.tripOption[0].saleTotal;
+        var numPrice = parseInt((price.slice(3,price.length)));
+        option.price = numPrice;
+
+        option.cityName = c[option.destination];
         console.log('Option:', option);
         result.push(option);
       };
 
       $scope.trips = result;
+      $scope.predicate = '';
+      $scope.number = 5000;
     });
   }]);
