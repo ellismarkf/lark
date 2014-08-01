66894445d937dbac671887cd7c93cd622e9f019a
Mark Ellis <ellismarkf@gmail.com>
2014-08-01 09:52:54 -0500

    add distance and duration attributes on trip objects

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,14 +2,24 @@
 
 var vultureControllers = angular.module('vultureControllers', ['ngResource']);
 
-vultureControllers.controller('searchCtlr', ['$scope', '$resource', 'Destination', 'CityCode',
-  function($scope, $resource, Destination, CityCode) {
+vultureControllers.controller('')
+
+vultureControllers.controller('searchCtlr', ['$scope', '$rootScope', '$resource', 'Destination', 'CityCode',
+  function($scope, $rootScope, $resource, Destination, CityCode) {
+
+    $rootScope.test = "this is a test of rootScope";
+
+    $rootScope.secondTest = {
+      val: "another test of rootScope variables"
+    }
+    $scope.test = $rootScope.secondTest.val;
+
 
     var apiPromise = $resource('api-key.json');
     apiPromise.get().$promise.then(function (key){
-      console.log('api key promise: ', key);
+
       var apiKey = key["API_KEY"];
-      console.log('api key object: ', apiKey);
+
       // $scope.key = apiKey;
       // return $http.get(ulr&key={apiKey});
     }).then(function (response) {
@@ -22,38 +32,22 @@ vultureControllers.controller('searchCtlr', ['$scope', '$resource', 'Destination
       for(var city in cities){
         c[city] = cities[city];
       }
-      console.log("cities: ", c);
       return c;
     });
 
 
-    // Destination.query().$promise.then(function (tripData) {
-    //   console.log('Trip data:', tripData);
+    // form.onsubmit(){
+    //   $http({
+    //     type: post
+    //     url: google api
+    //     param
+    //   })
+    // }
 
-    //   var result = [];
-    //   // Massage tripData and put into result
-
-    //   for(var i = 0; i < tripData.length; i += 1) {
-    //     var destinationPath = tripData[i].trips.tripOption[0].pricing[0].fare;
-    //     var option = {};
-    //     option.destination = destinationPath[destinationPath.length - 1].destination;
-    //     var price = tripData[i].trips.tripOption[0].saleTotal;
-    //     var numPrice = parseInt((price.slice(3,price.length)));
-    //     option.price = numPrice;
-
-    //     option.cityName = c[option.destination];
-    //     console.log('Option:', option);
-    //     result.push(option);
-    //   };
-
-    //   $scope.trips = result;
-    //   $scope.predicate = '';
-    //   $scope.number = 5000;
-    // });
   }]);
 
-vultureControllers.controller('resultsCtlr', ['$scope', 'Destination', 'CityCode',
-  function($scope, Destination, CityCode){
+vultureControllers.controller('resultsCtlr', ['$scope', '$rootScope', 'Destination', 'CityCode',
+  function($scope, $rootScope, Destination, CityCode){
 
     var c = {};
 
@@ -61,25 +55,36 @@ vultureControllers.controller('resultsCtlr', ['$scope', 'Destination', 'CityCode
       for(var city in cities){
         c[city] = cities[city];
       }
-      console.log("cities: ", c);
       return c;
     });
 
     Destination.query().$promise.then(function (tripData) {
-      console.log('Trip data:', tripData);
 
       var result = [];
        // Massage tripData and put into result
 
       for(var i = 0; i < tripData.length; i += 1) {
-        var destinationPath = tripData[i].trips.tripOption[0].pricing[0].fare;
         var option = {};
+
+        var destinationPath = tripData[i].trips.tripOption[0].pricing[0].fare;
         option.destination = destinationPath[destinationPath.length - 1].destination;
+
         var price = tripData[i].trips.tripOption[0].saleTotal;
         var numPrice = parseInt((price.slice(3,price.length)));
         option.price = numPrice;
+
+        var segments = tripData[i].trips.tripOption[0].slice[0].segment;
+        var distance = 0;
+        for(var j = 0; j < segments.length; j++){
+          distance = segments[j].leg[0].mileage + distance;
+        }
+        option.distance = distance;
+
+        var duration = tripData[i].trips.tripOption[0].slice[0].duration;
+        option.duration = duration;
+
         option.cityName = c[option.destination];
-        console.log('Option:', option);
+
         result.push(option);
       };
 
