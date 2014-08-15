29697fa0d4ae8685778b5713b9a273922b99b0aa
Code <code@Marks-iMac.local>
2014-08-15 00:56:14 -0500

    clean up search ui code

--- app/js/search-ui.js
+++ app/js/search-ui.js
@@ -2,13 +2,11 @@ $('.droption').click(function(){
   var that = $(this)[0];
   var other = that.childNodes[3];
   var display = $(other).css("display");
-  console.log(display);
   if(display == 'none' && this.id == 'passengers') {
-    $(other).show();
+    $(other).slideDown("ease");
   } else if(display == 'none' && this.id == 'stay') {
-    $(other).show();
+    $(other).slideDown("ease");
   } else {
-    console.log("what's being hidden? ", other);
-    $(other).hide();
+    $(other).slideUp("ease");
   }
 });
