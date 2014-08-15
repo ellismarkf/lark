fa244af28e120c498fde8911c21ae1d1165a054b
Mark Ellis <ellismarkf@gmail.com>
2014-08-14 20:59:31 -0500

    add working show/hide select option interface

--- app/js/search-ui.js
+++ app/js/search-ui.js
@@ -1,13 +1,14 @@
 $('.droption').click(function(){
-  var hidden = $('.droption-menu').css("display");
-  var hasMenu = $(this);
-  console.log(this.id);
-  console.log(this.contains(div));
-  if(hidden == 'none' && this.id == 'passengers') {
-    $('#p').show();
-  } else if(this.id == 'stay') {
-    $('#s').show();
+  var that = $(this)[0];
+  var other = that.childNodes[3];
+  var display = $(other).css("display");
+  console.log(display);
+  if(display == 'none' && this.id == 'passengers') {
+    $(other).show();
+  } else if(display == 'none' && this.id == 'stay') {
+    $(other).show();
   } else {
-    $('.droption-menu').hide();
+    console.log("what's being hidden? ", other);
+    $(other).hide();
   }
 });
