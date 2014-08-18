fc7e32f208589ed272bf9a6384d404d7185bd20d
Mark Ellis <ellismarkf@gmail.com>
2014-08-18 18:00:52 -0500

    add collect search form values directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,7 +1,32 @@
 'use strict';
 var vultureDirectives = angular.module('vultureDirectives', []);
 
-vultureDirectives.factory('searchForm', ['$document', '$scope',
+vultureDirectives.directive('searchFormSubmitter', ['$document',
   function($document) {
+    return function(scope, element, attr) {
 
-  }]);
+      element.on('click', function(event){
+        event.preventDefault();
+        console.log('directive working');
+        collectInputData(scope.inputs);
+        console.log('directive finished');
+      });
+
+
+      function collectInputData(arrayOfInputs){
+        for(var i = 0; i < arrayOfInputs.length; i++){
+          if(arrayOfInputs[i].hasChildNodes()){
+            collectInputData(arrayOfInputs[i].children);
+          }
+          console.log(arrayOfInputs[i]);
+          if(arrayOfInputs[i].tagName == 'INPUT'){
+            var jsonString = JSON.stringify(arrayOfInputs[i].dataset);
+            scope.inputData.push(JSON.parse(jsonString));
+          }
+        }
+        console.log(scope.inputData);
+        return scope.inputData;
+      }
+
+    };
+}]);
